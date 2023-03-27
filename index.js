0912207bef8a18d9143daf973397b1d521150c64
Mike <mikemakol45@gmail.com>
2023-03-27 17:15:52 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Mike"));
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage("Mike", "JavaScript"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguageOptional("Mike"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguageOptional("Mike", "CSS"));
